bb676ca7db6eb69a09005878b5110301b8ec37ed
Zsolt Kiraly <zkiraly@Zsolts-Mac-Pro.local>
2013-01-24 21:46:22 -0800

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Zsolt Kiraly on 1/24/13.
+//  Copyright (c) 2013 Zsolt Kiraly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Zsolt Kiraly on 1/24/13.
+//  Copyright (c) 2013 Zsolt Kiraly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
